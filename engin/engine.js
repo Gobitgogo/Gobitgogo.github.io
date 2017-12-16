a67bef3a8a098a9ec70a94991c2f381eeab5c87e
Gobitgogo <34598000+Gobitgogo@users.noreply.github.com>
2017-12-16 13:24:30 +0200

    Update engine.js

--- engin/engine.js
+++ engin/engine.js
@@ -130,11 +130,16 @@ function load(url)
 	});
 
 }
-const keys = {"LEFT" : 37,
-              "RIGHT" : 39,
-		      "UP" : 38,
-			  "DOWN" : 40,
-			  "SPACE" : 32
+const keys = {"LEFT":37,
+              "RIGHT":39,
+		      "UP":38,
+			  "DOWN":40,
+			  "SPACE":32,
+	      "Q":81,
+	      "W":87,"E":69,"R":82,"T":84,"Y":89,
+	      "U":85,"I":73,"O":79,"P":80,"A":65,"S":83,"D":68,
+	      "F":70,"G":71,"H":72,"J":74,"K":75,"L":76,
+	      "Z":90,"X":88,"V":86,"B":66,"N":78,"M":77
            }
 let keyDown = {};
 this.GBT_KeyDown = function(keyName)
